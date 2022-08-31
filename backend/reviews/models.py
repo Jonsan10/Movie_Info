db9af044a6405ecb60b68041bfe7cbfcd50e752d
jonathan sanchez <sanchezjon480@gmail.com>
2022-08-31 14:11:02 -0700

    Created review model for review app

---
+++ backend/reviews/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from authentication.models import User
+
+# Create your models here.
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=500)
+    movie_id = models.CharField(max_length=500)
+    rating = models.IntegerField()
+    timestamp = models.DateTimeField()
+    title = models.CharField(max_length=500)
+
