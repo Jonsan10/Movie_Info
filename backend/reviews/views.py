bcd6084edf475cd310668e352e9c3434452d20d0
jonathan sanchez <sanchezjon480@gmail.com>
2022-09-01 01:47:22 -0700

    updated views.py to reflect Get request

--- backend/reviews/views.py
+++ backend/reviews/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Review
+from .serializers import ReviewSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_reviews(request):
+    reviews = Review.objects.all()
+    serializer = ReviewSerializer(reviews, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def make_review(request):
+    print(
+        'User', f"{request.user.id} {request.user.email} {request.user.username}")
+    
